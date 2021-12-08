4bec3b885ad79855c2f716690e0a3141b8e2628d
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-12-08 15:50:17 +0700

    Performance improvement

--- regressions/image-multinominal-logistic-regression/logistic-regression.js
+++ regressions/image-multinominal-logistic-regression/logistic-regression.js
@@ -25,7 +25,7 @@ class LogisticRegression {
       .div(features.shape[0])
       .mul(2);
 
-    this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
+    return this.weights.sub(slopes.mul(this.options.learningRate));
   }
 
   train() {
@@ -38,13 +38,18 @@ class LogisticRegression {
         const startIndex = j * this.options.batchSize;
         const { batchSize } = this.options;
 
-        const featureSlice = this.features.slice(
-          [startIndex, 0],
-          [batchSize - 1]
-        );
-        const labelSlice = this.labels.slice([startIndex, 0], [batchSize - 1]);
+        this.weights = tf.tidy(() => {
+          const featureSlice = this.features.slice(
+            [startIndex, 0],
+            [batchSize - 1]
+          );
+          const labelSlice = this.labels.slice(
+            [startIndex, 0],
+            [batchSize - 1]
+          );
 
-        this.gradientDescent(featureSlice, labelSlice);
+          return this.gradientDescent(featureSlice, labelSlice);
+        });
       }
 
       this.recordCost();
@@ -94,21 +99,23 @@ class LogisticRegression {
   }
 
   recordCost() {
-    const guesses = this.features.matMul(this.weights).softmax();
+    const cost = tf.tidy(() => {
+      const guesses = this.features.matMul(this.weights).softmax();
 
-    const termOne = this.labels.transpose().matMul(guesses.log());
+      const termOne = this.labels.transpose().matMul(guesses.add(1e-7).log());
 
-    const termTwo = this.labels
-      .mul(-1)
-      .add(1)
-      .transpose()
-      .matMul(guesses.mul(-1).add(1).log());
+      const termTwo = this.labels
+        .mul(-1)
+        .add(1)
+        .transpose()
+        .matMul(guesses.mul(-1).add(1).add(1e-7).log());
 
-    const cost = termOne
-      .add(termTwo)
-      .div(this.features.shape[0])
-      .mul(-1)
-      .arraySync();
+      return termOne
+        .add(termTwo)
+        .div(this.features.shape[0])
+        .mul(-1)
+        .arraySync();
+    });
 
     this.costHistory.unshift(cost);
   }
